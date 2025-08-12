31bd3cc3f4c1aa45d69ac912310ae61c6f4fd021
Julian Woo <julian.wooville@gmail.com>
2025-08-12 10:06:57 -0400

    Update main.js

--- main/main.js
+++ main/main.js
@@ -44,7 +44,7 @@ const path = require('path');
 const devMode = app.commandLine.hasSwitch('dev');
 app.commandLine.appendSwitch('disable-site-isolation-trials');
 
-const webstratesURL = 'https://videoplayground.xyz/fat-frog-72/';
+const webstratesURL = 'https://videoplayground.xyz/old-fireant-73/';
 // const ll = require('leader-line');
 
 let roomWindow = null;
